8a9cd8f365931f8c822db66ce05305fb5c905d21
Vinicius <viniciusrmsouza@gmail.com>
2024-11-14 11:51:01 +0100

    Add class ensemblerEntiter

--- reservoirG2/main.cpp
+++ reservoirG2/main.cpp
@@ -3,11 +3,12 @@
 #include "vecteur3d.h"
 #include "chose.h"
 #include "point.h"
+#include "ensembleentier.h"
 
 using namespace std;
 
 int main(int argc, char *argv[]){
-
+/*
     Reservoir *monReservoir = new Reservoir(5);
 
     cout << "Clasee reservoir" << endl;
@@ -46,6 +47,67 @@ int main(int argc, char *argv[]){
     cout << "Classe point" << endl;
     Point* tableauPoints = new Point[4];
     delete[] tableauPoints;
+
+
+    return 0;
+*/
+    /*
+     * Exercice 3
+    EnsembleEntier *monEnsemble = new EnsembleEntier();
+
+    cout << monEnsemble->getCardinal() << endl;
+    monEnsemble->addElement(3);
+
+    cout << monEnsemble->getCardinal() << endl;
+    monEnsemble->addElement(3);
+
+    cout << monEnsemble->getCardinal() << endl;
+    monEnsemble->addElement(7);
+
+    cout << monEnsemble->getCardinal() << endl;
+    monEnsemble->addElement(2);
+
+    cout << monEnsemble->getCardinal() << endl;
+    monEnsemble->addElement(23);
+
+    cout << monEnsemble->appartient(2) << endl;
+    */
+/*
+ * Exercice 4
+    EnsembleEntier conjunto(5);
+
+    conjunto.addElement(3);
+    conjunto.addElement(5);
+    conjunto.addElement(3);
+    conjunto.addElement(7);
+    conjunto.addElement(9);
+    conjunto.addElement(11);
+
+    int numero = 5;
+    if (conjunto.appartient(numero)) {
+        std::cout << "O número " << numero << " pertence ao conjunto." << std::endl;
+    } else {
+        std::cout << "O número " << numero << " não pertence ao conjunto." << std::endl;
+    }
+
+    std::cout << "Cardinal final: " << conjunto.getCardinal() << std::endl;
+*/
+
+    EnsembleEntier conjunto(5);
+
+    conjunto.addElement(3);
+    conjunto.addElement(5);
+    conjunto.addElement(7);
+    conjunto.addElement(9);
+
+    std::cout << "Elementos do conjunto: ";
+    conjunto.init();
+
+    while (conjunto.existe()) {
+        std::cout << conjunto.prochain() << " ";
+    }
+
+    std::cout << "\nCardinal final: " << conjunto.getCardinal() << std::endl;
 
     return 0;
 }
